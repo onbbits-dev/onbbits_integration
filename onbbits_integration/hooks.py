af488101fccec66196da6b6a2509607f00f9c8ff
Krishna Kangad <krishna@arkayapps.com>
2026-02-10 10:14:54 +0000

    feat: improve OnBBits WhatsApp settings and add fixtures

--- onbbits_integration/hooks.py
+++ onbbits_integration/hooks.py
@@ -255,4 +255,13 @@ scheduler_events = {
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
-fixtures = ["Template Category"]
+fixtures = [
+    "Template Category",
+    {
+        "dt": "Workspace",
+        "filters": [
+            ["name", "=", "OnBBits"]
+        ]
+    }
+]
+
--- onbbits_integration/onbbits_integration/doctype/onbbits_wa_setting/onbbits_wa_setting.js
+++ onbbits_integration/onbbits_integration/doctype/onbbits_wa_setting/onbbits_wa_setting.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2025, It provides complete, centralized control over WhatsApp communication inside Frappe, enabling businesses to automate customer messaging with accuracy, reliability, and real-time synchronization. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("OnBBits WA Setting", {
-// 	refresh(frm) {
+frappe.ui.form.on("OnBBits WA Setting", {
+    refresh(frm) {
+        if (!frm.doc.api_key) {
+            frm.set_intro(__(
+                `<b>Let’s Get You Connected</b><br>
+                Don’t have an account yet?
+                <a href="https://app.onbbits.io/register" target="_blank">
+                    Register here
+                </a> to get started.`
+            ));
+        } else {
+            frm.set_intro("");
+        }
+    },
+});
 
-// 	},
-// });
